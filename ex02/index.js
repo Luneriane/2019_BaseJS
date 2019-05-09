e0b7538235c1c83fc5e2c3434518576ff6f9fd3c
Luneriane <luneriane@gmail.com>
2019-05-09 10:12:08 +0200

    ex04

--- ex02/index.js
+++ ex02/index.js
@@ -1,2 +1,9 @@
 const valeurRandom = Math.random() > 0.5;
+
 valeurRandom ? console.log("C’est vrai") : console.log("C’est faux");
+
+if (valeurRandom) {
+    console.log("C’est vrai")
+} else {
+    console.log("C’est faux");
+}
---
+++ ex04/index.js
@@ -0,0 +1,25 @@
+var myVar = 0;
+while (myVar <= 10) {
+    console.log(myVar);
+    myVar++;
+}
+
+var a = 0;
+var b = 31;
+while (a<20) {
+    console.log(a*b);
+    a++;
+}
+
+var c = 100;
+var d = 62;
+while (c <= 10) {
+    console.log(c*d);
+    c--;
+}
+
+var e = 1;
+while (e < 10) {
+    console.log(e);
+    e+=e/2;
+}
